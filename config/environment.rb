f2f6331d301b7c009ea51169d265d103456c9661
C.J <bordeleau.cj@gmail.com>
2012-09-01 16:27:51 -0400

    adding mailer support

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,8 @@
 #set facebook app ID and secret
 ENV['FACEBOOK_APP_ID'] = '113351525479090'
 ENV['FACEBOOK_APP_SECRET'] = '7e9b5dfe60bde6f812525886cc662041'
-
-config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :address  => "mail.startupmatchup.org",
-  :port  => 26,
-  :user_name  => 'no-reply@startupmatchup.org',
-  :password  => 'nlC38oJ@v=o3',
-  :authentication  => :login
-}
-
-config.action_mailer.raise_delivery_errors = true
+ENV['EMAIL_USERNAME'] = 'no-reply@startupmatchup.org'
+ENV['EMAIL_PASSWORD'] = 'nlC38oJ@v=o3'
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,18 @@ StartupMatchup::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address: "mail.startupmatchup.org",
+     port: 26,
+     domain: "startupmatchup.org",
+     authentication: "plain",
+     enable_starttls_auto: false,
+     user_name: ENV['EMAIL_USERNAME'],
+     password: ENV['EMAIL_PASSWORD']
+   }
+  
+  config.action_mailer.raise_delivery_errors = true
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
